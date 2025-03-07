7ede0a05d1cbb5fd71a89673bf2b781c25d54437
Tesfahun03 <tesaf1035@gmail.com>
2025-03-07 10:53:12 +0300

    fixing import statment

--- src/model_training.py
+++ src/model_training.py
@@ -5,7 +5,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
 from xgboost import XGBRegressor
-from sklearn.model_split import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from typing import Tuple, Union
 
